472b82861957dd11ea5379635e0c612c7cf10c23
benjaminleezhiyuan <113172138+benjaminleezhiyuan@users.noreply.github.com>
2022-11-06 21:14:17 +0800

    Add files via upload

---
+++ assi09/q.c
@@ -0,0 +1,12 @@
+// TODO: File documentation block required
+
+// TODO: Include all necessary C standard library headers 
+#include <stdio.h>
+#include "q.h"
+// TODO: Definitions of functions declared in q.h go here ...
+int read_data(char const *file_name, struct Tsunami *arr, int max_cnt)
+{   
+    char info
+    FILE *open = (file_name,"r")
+    fscanf("%d %d %d %d %0.2f %s",arr->day,arr->month,arr->year,arr->fatalities,arr->maxwave,arr->location);
+}
---
+++ assi09/q.h
@@ -0,0 +1,52 @@
+#ifndef Q_H
+#define Q_H
+
+// TODO: File documentation block required
+
+// TODO: Include only those C standard library headers that declare/define names used in functions declared below
+
+// TODO: A function documentation block provides information that is crucial
+// for clients to correctly use the function. Provide a documention block
+// for each declared function that contains the following information:
+// What does the function do?
+// What are the (types) of the parameters?
+// What is the (type) of value returned by function?
+// Is there any particular input that will break the function?
+
+// TODO: Now, declare the necessary function.
+
+// TODO: Repeat function documentation block plus function declaration for remaining functions
+typedef int numbers;
+typedef double height;
+typedef unsigned char geo[80];
+struct Tsunami
+{
+    numbers day;
+    numbers month;
+    numbers year;
+    numbers fatalities;
+    height maxwave;
+    geo location; 
+};
+
+
+/*
+typedef struct
+{
+    int day;
+    int month;
+    int year;
+    int fatalities;
+    double height;
+    unsigned char location;
+}Tsunami;
+*/
+
+int read_data(char const *file_name, struct Tsunami *arr, int max_cnt);
+void print_info(struct Tsunami const *arr, int size, FILE *out_stm);
+double get_max_height(struct Tsunami const *arr, int size);
+double get_average_height(struct Tsunami const *arr, int size);
+void print_height(struct Tsunami const *arr, int size, double height, FILE *out_stm);
+
+#endif
+
