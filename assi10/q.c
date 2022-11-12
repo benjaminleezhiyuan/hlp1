b098f2253f768dd69fdf7bbba8d55bf65e2445ba
benjamin <benjaminzhiyuan.lee@digipen.edu>
2022-11-13 01:34:28 +0800

    stuff

--- assi10/q.c
+++ assi10/q.c
@@ -10,6 +10,24 @@ Brief
 
 #include "q.h" // we're including q.h here because we want to apply the DRY principle [see specs for additional info]
 
-// TODO: Include all necessary C standard library headers 
-
+// TODO: Include all necessary C standard library headers
+#include <stdio.h>;
 // TODO: Definitions of functions declared in q.h go here ...
+double *read_data(char const *file_name, int *ptr_cnt)
+{
+    FILE *read = fopen(file_name, "r");
+    if (read == NULL)
+    {
+        return NULL;
+    }
+    int count = 1;
+    char temp;
+    while (temp = fgetc(read) != EOF)
+    {
+        if (temp = ' ')
+        {
+            count++;
+        }
+    }
+    double *heap = malloc(count * sizeof(double));
+}
