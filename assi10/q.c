c341ff533fc0220502adff0bcd503c6ad21f1adc
benjamin <benjaminzhiyuan.lee@digipen.edu>
2022-11-13 01:57:16 +0800

    asdf

--- assi10/q.c
+++ assi10/q.c
@@ -11,7 +11,8 @@ Brief
 #include "q.h" // we're including q.h here because we want to apply the DRY principle [see specs for additional info]
 
 // TODO: Include all necessary C standard library headers
-#include <stdio.h>;
+#include <stdio.h>
+#include <stdlib.h>
 // TODO: Definitions of functions declared in q.h go here ...
 double *read_data(char const *file_name, int *ptr_cnt)
 {
@@ -20,14 +21,22 @@ double *read_data(char const *file_name, int *ptr_cnt)
     {
         return NULL;
     }
-    int count = 1;
-    char temp;
-    while (temp = fgetc(read) != EOF)
+    int count = 0;
+    char ch;
+    while ((ch = fgetc(read)) != EOF)
     {
-        if (temp = ' ')
+        if (ch == ' ' || ch == '\t' || ch == '\n' || ch == '\0')
         {
             count++;
         }
     }
+    *ptr_cnt = count;
     double *heap = malloc(count * sizeof(double));
+    double num = 0;
+    while (fscanf(read, "%lf", &num) != EOF)
+    {
+        *heap = num;
+        heap++;
+    }
+    return heap;
 }
